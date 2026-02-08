28b62fcbd6500464f4a5c3bd45b760fa799d4d43
shainyguy <shainyguy@gmail.com>
2026-02-08 22:05:23 +0300

    Update init.py

--- bot/services/init.py
+++ bot/services/init.py
@@ -1,9 +1,1 @@
-from bot.services.gigachat import gigachat_service
-from bot.services.payment import payment_service
-from bot.services.scheduler import scheduler_service
 
-__all__ = [
-    "gigachat_service",
-    "payment_service",
-    "scheduler_service",
-]
